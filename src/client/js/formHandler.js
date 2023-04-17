4b03134bf9853e87383147b5aefad2d7a628c2d4
bjoeas <75702116+bjoeas@users.noreply.github.com>
2023-04-17 10:03:04 +0200

    Setting up the API

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,19 +1,53 @@
 function handleSubmit(event) {
+    // event.preventDefault()
+    //
+    // // check what text was put into the form field
+    // let formText = document.getElementById('name').value
+    // // checkForName(formText)
+    //
+    // // TODO: Add the Client to the below JS function
+    // Client.checkForName(formText)
+    //
+    // console.log("::: Form Submitted :::")
+    // fetch('http://localhost:8081/test')
+    // .then(res => res.json())
+    // .then(function(res) {
+    //     document.getElementById('results').innerHTML = res.message
+    // })
+
     event.preventDefault()
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    // checkForName(formText)
 
-    // TODO: Add the Client to the below JS function
-    Client.checkForName(formText)
+    console.log(formText);
+    postData('/meaningCloud', {url: formText});
+    // postData('/meaningCloud', formText);
+    // postData('/meaningCloud', 'https://www.seo-analyse.com/seo-lexikon/b/blogpost/');
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+    // TODO: URL Checker
+
+    // TODO: Update UI
 }
 
-export { handleSubmit }
+const postData = async (url = '', data = {}) => {
+    try {
+        const res = await fetch(url, {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data), // body data type must match "Content-Type" header
+        });
+
+        const newData = await res.json();
+        console.log('postData');
+        console.log(newData);
+        return newData;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export {handleSubmit}
--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,28 @@
-var path = require('path')
+const dotenv = require('dotenv');
+dotenv.config();
+
+// console.log(`Your API key is ${process.env.API_KEY}`);
+const baseURL = 'https://api.meaningcloud.com/sentiment-2.1';
+const apiKey = process.env.API_KEY
+
+// var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
 
+/* Middleware*/
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 // app.use(express.static('src/client'))
 app.use(express.static('dist'))
 
 console.log(__dirname)
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
@@ -22,3 +37,65 @@ app.listen(8081, function () {
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+app.post('/meaningCloud', async (req, res) => {
+
+    console.log(req.body);
+
+    // const formdata = new FormData();
+    // formdata.append("key", apiKey);
+    // formdata.append("url", req.body); // req.body.url
+    // formdata.append("lang", "auto");  // 2-letter code, like en es fr ...
+    //
+    // const requestOptions = {
+    //     method: 'POST',
+    //     body: formdata,
+    //     redirect: 'follow'
+    // };
+
+    try {
+        // const response = fetch(baseURL, requestOptions)
+        const response = await fetch(`${baseURL}?key=${apiKey}&lang=auto&url=${req.body.url}`);
+
+        // console.log(response.status);
+        // console.log(response);
+
+        const data = await response.json();
+
+        console.log(data);
+
+        res.send(data);
+
+    } catch (error) {
+        console.error(error);
+        // TODO: Fehlermeldung im HTML ausgeben!
+    }
+
+    // const response = fetch(baseURL, requestOptions)
+    //     .then(response => ({
+    //         status: response.status,
+    //         body: response.json()
+    //     }))
+    //     .then(({status, body}) => console.log(status, body))
+    //     .catch(error => console.log('error', error));
+})
+
+// Example MeaningCloud
+// const formdata = new FormData();
+// formdata.append("key", "YOUR API KEY");
+// formdata.append("txt", "YOUR TEXT HERE");
+// formdata.append("lang", "TEXT LANGUAGE HERE");  // 2-letter code, like en es fr ...
+//
+// const requestOptions = {
+//     method: 'POST',
+//     body: formdata,
+//     redirect: 'follow'
+// };
+//
+// const response = fetch("https://api.meaningcloud.com/sentiment-2.1", requestOptions)
+//     .then(response => ({
+//         status: response.status,
+//         body: response.json()
+//     }))
+//     .then(({ status, body }) => console.log(status, body))
+//     .catch(error => console.log('error', error));
